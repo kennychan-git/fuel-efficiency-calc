c85838b9d0e5e8167e9e3b3389cda75172fbab00
kennychan <129039471+kennychan-git@users.noreply.github.com>
2023-04-08 08:39:20 +0800

    Update fuel.py
    added mpg and convert to km/L, disregard pump price and fuel price here

--- fuel.py
+++ fuel.py
@@ -3,31 +3,53 @@ from tkinter import ttk
 from ttkthemes import ThemedTk 
 
 def calculate_efficiency():
-    liters = float(entry_liters.get()) if entry_liters.get() else None
-    km = float(entry_km.get()) if entry_km.get() else None
-    fuel_price = float(entry_fuel_price.get()) if entry_fuel_price.get() else None
-    pump_price = float(entry_pump_price.get()) if entry_pump_price.get() else None
+    mpg = float(mpg_label.get()) if mpg_label.get() else None
+    if mpg:
+        km_per_gallon = 1.60934 / 3.78541 * mpg
+        liters = 100 / km_per_gallon
+        km = 100
+        #fuel_price = float(entry_fuel_price.get()) if entry_fuel_price.get() else None
+        #pump_price = liters * fuel_price
+        
+        result_l_100km = (100 * liters) / km
+        #result_rm_km = (pump_price) / km
+        result_km_l = km / liters
+    
+        result_liters_var.set(f"Liters: {liters:.2f}")
+        result_km_var.set(f"Kilometers: {km:.2f}")
+        #result_fuel_price_var.set(f"Fuel price (RM/L): {fuel_price:.2f}")
+        #result_pump_price_var.set(f"Pump price (RM): {pump_price:.2f}")
+        result_l_100km_var.set(f"L/100 km: {result_l_100km:.2f}")
+        #result_rm_km_var.set(f"RM/km: {result_rm_km:.2f}")
+        result_km_l_var.set(f"km/L: {result_km_l:.2f}")
+    else:
+        liters = float(entry_liters.get()) if entry_liters.get() else None
+        km = float(entry_km.get()) if entry_km.get() else None
+        fuel_price = float(entry_fuel_price.get()) if entry_fuel_price.get() else None
+        pump_price = float(entry_pump_price.get()) if entry_pump_price.get() else None
 
-    if not liters:
-        liters = pump_price / fuel_price
-    if not km:
-        km = (100 * liters) / fuel_price
-    if not fuel_price:
-        fuel_price = pump_price / liters
-    if not pump_price:
-        pump_price = liters * fuel_price
+        if not liters:
+            liters = pump_price / fuel_price
+        if not km:
+            km = (100 * liters) / fuel_price
+        if not fuel_price:
+            fuel_price = pump_price / liters
+        if not pump_price:
+            pump_price = liters * fuel_price
 
-    result_l_100km = (100 * liters) / km
-    result_rm_km = (pump_price) / km
-    result_km_l = km / liters
+        result_l_100km = (100 * liters) / km
+        result_rm_km = (pump_price) / km
+        result_km_l = km / liters
+    
+        result_liters_var.set(f"Liters: {liters:.2f}")
+        result_km_var.set(f"Kilometers: {km:.2f}")
+        result_fuel_price_var.set(f"Fuel price (RM/L): {fuel_price:.2f}")
+        result_pump_price_var.set(f"Pump price (RM): {pump_price:.2f}")
+        result_l_100km_var.set(f"L/100 km: {result_l_100km:.2f}")
+        result_rm_km_var.set(f"RM/km: {result_rm_km:.2f}")
+        result_km_l_var.set(f"km/L: {result_km_l:.2f}")
 
-    result_liters_var.set(f"Liters: {liters:.2f}")
-    result_km_var.set(f"Kilometers: {km:.2f}")
-    result_fuel_price_var.set(f"Fuel price (RM/L): {fuel_price:.2f}")
-    result_pump_price_var.set(f"Pump price (RM): {pump_price:.2f}")
-    result_l_100km_var.set(f"L/100 km: {result_l_100km:.2f}")
-    result_rm_km_var.set(f"RM/km: {result_rm_km:.2f}")
-    result_km_l_var.set(f"km/L: {result_km_l:.2f}")
+
 
 # Replace the following line:
 # root = tk.Tk()
@@ -80,36 +102,40 @@ ttk.Label(mainframe, text="Pump price (RM):",font=custom_font, foreground="blue"
 entry_pump_price = ttk.Entry(mainframe, width=7)
 entry_pump_price.grid(row=3, column=1, padx=5)
 
+ttk.Label(mainframe, text="MPG :",font=custom_font, foreground="blue", background="white").grid(row=4, column=0, sticky=(tk.E), padx=5)
+mpg_label = ttk.Entry(mainframe, width=7)
+mpg_label.grid(row=4, column=1, padx=5)
+
 calculate_button = ttk.Button(mainframe, text="Calculate", command=calculate_efficiency)
-calculate_button.grid(row=4, column=0, columnspan=4, pady=10)
+calculate_button.grid(row=5, column=0, columnspan=4, pady=10)
 
 result_liters_var = tk.StringVar()
 result_liters_label = ttk.Label(mainframe, textvariable=result_liters_var,font=custom_font2)
-result_liters_label.grid(row=5, column=0, columnspan=4)
+result_liters_label.grid(row=6, column=0, columnspan=4)
 
 result_km_var = tk.StringVar()
 result_km_label = ttk.Label(mainframe, textvariable=result_km_var,font=custom_font2)
-result_km_label.grid(row=6, column=0, columnspan=4)
+result_km_label.grid(row=7, column=0, columnspan=4)
 
 result_fuel_price_var = tk.StringVar()
 result_fuel_price_label = ttk.Label(mainframe, textvariable=result_fuel_price_var,font=custom_font2)
-result_fuel_price_label.grid(row=7, column=0, columnspan=4)
+result_fuel_price_label.grid(row=8, column=0, columnspan=4)
 
 result_pump_price_var = tk.StringVar()
 result_pump_price_label = ttk.Label(mainframe, textvariable=result_pump_price_var,font=custom_font2)
-result_pump_price_label.grid(row=8, column=0, columnspan=4)
+result_pump_price_label.grid(row=9, column=0, columnspan=4)
 
 result_l_100km_var = tk.StringVar()
 result_l_100km_label = ttk.Label(mainframe, textvariable=result_l_100km_var,font=custom_font2)
-result_l_100km_label.grid(row=9, column=0, columnspan=4)
+result_l_100km_label.grid(row=10, column=0, columnspan=4)
 
 result_rm_km_var = tk.StringVar()
 result_rm_km_label = ttk.Label(mainframe, textvariable=result_rm_km_var,font=custom_font2)
-result_rm_km_label.grid(row=10, column=0, columnspan=4)
+result_rm_km_label.grid(row=11, column=0, columnspan=4)
 
 result_km_l_var = tk.StringVar()
 result_km_l_label = ttk.Label(mainframe, textvariable=result_km_l_var,font=custom_font2)
-result_km_l_label.grid(row=11, column=0, columnspan=4)
+result_km_l_label.grid(row=12, column=0, columnspan=4)
 
 
 # Add an image to a Label or Button (optional)
